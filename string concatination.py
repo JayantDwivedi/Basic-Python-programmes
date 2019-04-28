18fcc3e2894b35a81bf88300a845637f4d828b06
JayantDwivedi <49179672+JayantDwivedi@users.noreply.github.com>
2019-04-28 11:39:13 +0530

    Update string concatination.py

--- string concatination.py
+++ string concatination.py
@@ -1,3 +1,6 @@
-x=input("ENter your name\t")
-y=input("ENter your last name\t")
-print(x+y)
+x=input("ENter your name\t")    # let x="Hello"
+y=input("ENter your last name\t")   # y= "World"
+print(x+y)    # it will return you like this :: HelloWorld 
+# if you try to concatinate string in such a way then there is no space between the two strings.
+print(x,y)  # it will return you like this :: Hello World
+'''if you try to concatinate string like this then there is space between strings'''
